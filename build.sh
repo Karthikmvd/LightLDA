f8ef181aa78523a27f6c5ced03939cc5312cb0d4
Karthikmvd <makkthic@in.ibm.com>
2018-03-21 19:15:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # build lightlda
 
 git clone -b multiverso-initial git@github.com:Microsoft/multiverso.git
+tar cp /home/rhel/karthi/lightlda/light_lda_fix.tgz .
+tar -xvzf light_lda_fix.tgz
 
 cd multiverso
 cd third_party
